f145cb8e3a7687420e5809c9a494d125c41a023e
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-05 16:49:55 -0400

    Update app.ts

--- TypeScript Practicing/app.ts
+++ TypeScript Practicing/app.ts
@@ -1,1 +1,9 @@
-const names =['Max', 'Manu']
+const names: Array<string|number> =['Max', 'Manuel'];
+const promise: Promise<sting> = new Promise((resolve, reject)=> {
+    setTimeout(()=>{
+        resolve('this is done');
+    }, 3000)
+});
+promise.then(data => {
+    data.split(' ');
+})

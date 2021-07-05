8d9505ef5fe3207cfbcddcb39e6fd5466fc07dae
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-05 10:54:17 -0400

    cxv

--- TypeScript Practicing/app.js
+++ TypeScript Practicing/app.js
@@ -1,15 +1,3 @@
 "use strict";
-class Personclass {
-    constructor(n, a) {
-        this.name = n;
-        this.age = a;
-    }
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name + ' ' + this.age + ' years old!');
-    }
-}
-let user1;
-user1 = new Personclass('max', 18);
-user1.greet('Hi there');
-console.log(user1);
+const names = ['Max', 'Manu'];
 //# sourceMappingURL=app.js.map

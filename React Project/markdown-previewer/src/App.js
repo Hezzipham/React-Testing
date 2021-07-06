c78b82812d4e77f2fb67f26d7ce9dde46b778799
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-06 16:39:22 -0400

    c

--- React Project/markdown-previewer/src/App.js
+++ React Project/markdown-previewer/src/App.js
@@ -1,21 +1,48 @@
-
 import React from "react";
 import Badge from "react-bootstrap/Badge";
 
 
-export default class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  
+  render() {
+
+    return (
       <div className="App">
         <div className="container">
-          <h1>
-          <Badge className="text-align-center" variant="light">
-          Markdown Previewer
-          </Badge>
-          </h1>
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="text-align-center" variant="light">
+                  Markdown Previewer
+                </Badge>
+              </h1>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Preview
+                  </Badge>
+                </h4>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
-
 }
+
+export default <App />
